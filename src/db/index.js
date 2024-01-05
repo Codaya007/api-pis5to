b6996fee0e248c6a64f4987a0537e6f83059d728
vivi <viviana.calva@lumation.co>
2024-01-05 00:54:53 -0500

    Adding plugins for soft delete and to manage external ids Adding CRUD for weatherData

--- src/db/index.js
+++ src/db/index.js
@@ -3,6 +3,15 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    // Configuración global para excluir el campo _id al convertir a JSON
+    mongoose.set("toJSON", {
+      transform: (doc, ret) => {
+        // Elimino campos delicados por seguridad
+        delete ret._id; // Eliminar el campo _id
+        delete ret.__v; // Eliminar el campo __v si es necesario
+      },
+    });
+
     const connection = await mongoose.connect(process.db.url);
 
     console.info(`MongoDB Connected: ${connection.connection.host}`);
