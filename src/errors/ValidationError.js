947ff5296b96a25eb4adb5b1f3d2995633859a27
LeoMaza <dalton.l.maza@unl.edu.ec>
2024-01-07 14:47:19 -0500

    solving errors on merge login

--- src/errors/ValidationError.js
+++ src/errors/ValidationError.js
@@ -1,14 +1,5 @@
 class ValidationError extends Error {
-<<<<<<< HEAD
-    constructor(message) {
-      super(message);
-      this.name = "ValidationError";
-      this.status = 400;
-    }
-  }
-  
-  module.exports = ValidationError;
-=======
+
   constructor(message) {
     super(message);
     this.name = "ValidationError";
@@ -17,4 +8,4 @@ class ValidationError extends Error {
 }
 
 module.exports = ValidationError;
->>>>>>> df696440bc22df7207ee644e58b30dc719bf2088
+
